33e3af9d3b7724f8bb7a4b4689f286cd7cfcdea9
daizylogik <vladimir@daizylogik.com>
2018-09-30 10:44:23 -0700

    code complete

--- plot1.R
+++ plot1.R
@@ -1,1 +1,25 @@
 library(tidyverse)
+
+setwd("~/Documents/datascience/eda/CourseProject2")
+pngFile <- "plot1.png"
+
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# sum the emissions from NEI, by year
+agg <-  aggregate(Emissions ~ year, NEI, sum)
+
+# show emissions in mega-tons
+agg$Emissions <- agg$Emissions / 10^6
+
+# open png device
+png(filename = pngFile, width = 480, height = 480)
+
+# plot
+barplot(height = agg$Emissions, horiz = FALSE, names.arg=agg$year, width = 1, main = "Emissions by Year",
+        xlab = "Year", ylab = "Emissions (Megatons)")
+
+# save png file
+dev.off()
+
